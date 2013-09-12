287722976ebb32cf2b02e24ceb36d3633230f785
masa <masa@10.200.56.187.d.tokyo.iiji.jp>
2013-09-12 20:16:41 +0900

    ok

--- vxlan.c
+++ vxlan.c
@@ -10,5 +10,5 @@ int cmp_data(TABLE_DATA_TYPE *d1, TABLE_DATA_TYPE *d2)
 	struct ether_addr hw_addr1 = d1->hw_addr;
 	struct ether_addr hw_addr2 = d2->hw_addr;
 
-	return cmp_mac(hw_addr1->ether_addr_octet, hw_addr2->ether_addr_octet);
+	return cmp_mac(hw_addr1.ether_addr_octet, hw_addr2.ether_addr_octet);
 }
