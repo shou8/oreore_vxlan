05e03a1822961e7807de38e7a11f80a1f55d125f
root <root@exp1>
2014-02-03 15:48:04 +0900

    ok

--- net.c
+++ net.c
@@ -53,14 +53,13 @@ typedef struct _vxlan_h_ {
  */
 int outer_loop(void) {
 
-	int buf_len;
-	//int len;
-	//char *p;
-	char buf[BUF_SIZE], *bp;
+	char buf[BUF_SIZE], *bp = buf;
+	int len;
+	int rlen = sizeof(buf) - 1;
+
 	struct sockaddr_in src;
 	socklen_t addr_len = sizeof(src);
 
-	bp = buf;
 	vxlan.usoc = init_udp_sock(VXLAN_PORT);
 	if (vxlan.usoc < 0) log_cexit("outer_loop.socket: Bad descripter\n");
 
@@ -72,17 +71,17 @@ int outer_loop(void) {
 
 	while (1) {
 
-		if ((buf_len = recvfrom(vxlan.usoc, buf, sizeof(buf)-1, 0,
+		if ((len = recvfrom(vxlan.usoc, buf, rlen, 0,
 						(struct sockaddr *)&src, &addr_len)) < 0)
 			log_perr("recvfrom");
 
-printf("outer raw buf_len: %d\n", buf_len);
+printf("outer raw buf_len: %d\n", len);
 
 		vxlan_h *vh = (vxlan_h *)buf;
 		bp = buf + sizeof(vxlan_h);
-		buf_len -= sizeof(vxlan_h);
+		len -= sizeof(vxlan_h);
 
-printf("outer aft buf_len: %d\n", buf_len);
+printf("outer aft buf_len: %d\n", len);
 
 #ifdef DEBUG
 		if (get_status())
@@ -93,14 +92,11 @@ printf("outer aft buf_len: %d\n", buf_len);
 		if (v == NULL) continue;
 
 		struct ether_header *eh = (struct ether_header *)bp;
-
-		/* Store VTEP information */
 		if (ntohs(eh->ether_type) == ETHERTYPE_ARP) {
 			add_data(v->table, eh->ether_shost, src.sin_addr.s_addr);
 		}
 
-//		send(v->tap.sock, bp, buf_len, MSG_DONTWAIT);
-		write(v->tap.sock, bp, buf_len); 
+		write(v->tap.sock, bp, len); 
 
 #ifdef DEBUG
 		if (get_status())
@@ -111,8 +107,6 @@ printf("outer aft buf_len: %d\n", buf_len);
 		 * And this have "while loop", so we have to use "thread_cancel" to stop this.
 		 * But, we don't have to worry to use it, because pthread's "canceltype" is "DEFERRED" by default.
 		 */
-
-//		write(v->tap.sock, bp, buf_len); 
 	}
 
 	/*
@@ -135,15 +129,6 @@ int inner_loop(vxlan_i *v) {
 	struct sockaddr_in src;
 	socklen_t addr_len = sizeof(src);
 
-	/* For UDP socket declaration (Write) */
-/*
-	if (vxlan.usoc < 0) vxlan.usoc = init_udp_sock(VXLAN_PORT);
-	if (vxlan.usoc < 0) {
-		log_crit("inner_loop.socket: Bad descripter\n");
-		return -1;
-	}
-*/
-
 	struct sockaddr_in dst;
 	dst.sin_port = htons(VXLAN_PORT);
 
@@ -156,8 +141,6 @@ int inner_loop(vxlan_i *v) {
 	}
 
 	mac_tbl *data;
-
-	/* Initialize vxlan header */
 	vxlan_h *vh = (vxlan_h *)buf;
 
 	while (1) {
@@ -169,10 +152,11 @@ int inner_loop(vxlan_i *v) {
 		memset(vh, 0, sizeof(vxlan_h));
 		vh->flag = VXLAN_FLAG_MASK;
 		memcpy(vh->vni, v->vni, VNI_BYTE);
+		len += sizeof(vxlan_h);
 
 printf("inner max buf_len: %d\n", rlen);
-printf("inner raw buf_len: %d\n", len);
-printf("inner aft buf_len: %d\n", sizeof(vxlan_h)+len);
+printf("inner raw buf_len: %d\n", len-sizeof(vxlan_h));
+printf("inner aft buf_len: %d\n", len);
 
 #ifdef DEBUG
 		if (get_status())
@@ -188,15 +172,15 @@ printf("inner aft buf_len: %d\n", sizeof(vxlan_h)+len);
 
 		data = find_data(v->table, eh->ether_dhost);
 		if (ntohs(eh->ether_type) == ETHERTYPE_ARP || data == NULL) {
-			dst.sin_addr.s_addr = vxlan.mcast_addr;
-			if (sendto(vxlan.usoc, buf, sizeof(vxlan_h)+len, MSG_DONTWAIT, (struct sockaddr *)&dst, sizeof(struct sockaddr)) < 0)
+			dst.sin_addr.s_addr = htonl(vxlan.mcast_addr);
+			if (sendto(vxlan.usoc, buf, len, MSG_DONTWAIT, (struct sockaddr *)&dst, sizeof(struct sockaddr)) < 0)
 				log_perr("inner_loop.sendto");
 printf("mcast: %d\n", vxlan.usoc);
 			continue;
 		}
 
 		dst.sin_addr.s_addr = htonl(data->vtep_addr);
-		if (sendto(vxlan.usoc, buf, sizeof(vxlan_h)+len, MSG_DONTWAIT, (struct sockaddr *)&dst, sizeof(struct sockaddr)) < 0)
+		if (sendto(vxlan.usoc, buf, len, MSG_DONTWAIT, (struct sockaddr *)&dst, sizeof(struct sockaddr)) < 0)
 			log_perr("inner_loop.sendto");
 printf("ucast: %d\n", vxlan.usoc);
 	}
